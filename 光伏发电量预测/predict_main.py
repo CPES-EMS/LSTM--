e03500736463e995419e4339a7552d52e49d4e0c
jiahao666-chen <88417432+jiahao666-chen@users.noreply.github.com>
2023-12-22 20:16:02 +0800

    Update predict_main.py

--- 光伏发电量预测/predict_main.py
+++ 光伏发电量预测/predict_main.py
@@ -1,23 +1,100 @@
+import mysql.connector
+import pytz
 import datetime
-import sys
-
+from datetime import datetime
 import traceback
+
+from pydantic.datetime_parse import timedelta
+
 from LSTM_validation import Lstm_Net
-from config import window_label_size, logging
-from sql_lstm import GetAllDataFromDB, InsertData, UpdateData, GetNearestDataFromDB, IsExist
+from config import window_label_size, logging, els_sheet, hydrogen_sheet, hvac_sheet, output_sheet, env_sheet, \
+    dataaddress, port_num, user_name, password, datause
+from sql_lstm import GetAllDataFromDB, InsertData, UpdateData, GetNearestDataFromDB, GetPredictDataFromDB
 import pandas as pd
 import torch
 import numpy as np
 
 Lstm_Net.load_state_dict(torch.load("./output_results/model.pt"))  # 加载模型参数
+mydb = mysql.connector.connect(
+    host=dataaddress,  # 数据库主机地址
+    port=port_num,
+    user=user_name,  # 数据库用户名
+    passwd=password,  # 数据库密码
+    database=datause
+)
+
 
 try:
-    # 从电气系统数据库读入数据
+    # 从电气系统数据库读入光伏数据
     row_name1 = ['load_time', 'load_value']
-    raw_data1 = GetAllDataFromDB(position_name='a', position_id='1', device_name='b', device_id='2',
-                                 system_name='c',
-                                 system_id='3', row_name=','.join(row_name1), table_name='sheet1', time='load_time',
-                                 start_time=None)
+    device_name_list = ['DC/DC-1号DC/DC', 'DC/DC-2号DC/DC', 'DC/DC-3号DC/DC', 'DC/DC-4号DC/DC',
+                        'DC/DC-5号DC/DC', 'DC/DC-6号DC/DC']
+    position_name_list = ['1#总放电电量', '2#总放电电量', '3#总放电电量',
+                          '4#总放电电量', '5#总放电电量', '6#总放电电量']
+    raw_data1 = pd.DataFrame()
+    for i in range(0, len(device_name_list)):
+        temp_data1 = GetAllDataFromDB(position_name=position_name_list[i],
+                                      device_name=device_name_list[i],
+                                      system_name='光伏系统',
+                                      system_id='0101', row_name=','.join(row_name1),
+                                      table_name=els_sheet,
+                                      time='load_time',
+                                      start_time=None)
+        if i == 0:
+            raw_data1['load_time'] = temp_data1['load_time']
+            raw_data1[i] = temp_data1['load_value']
+        else:
+            raw_data1[i] = temp_data1['load_value']
+
+    # 求和得到总的光伏功率
+    raw_data1['load_value'] = raw_data1.iloc[:, 1:].sum(axis=1)
+    order = ['load_time', 'load_value']
+    raw_data1 = raw_data1[order]
+    raw_data1 = raw_data1[::-1]
+    raw_data1 = raw_data1.reset_index(drop=True)
+    print(raw_data1)
+
+    same_time = raw_data1['load_time']
+    # 从环境系统数据库读入天气预报数据
+    position_name_list1 = ['室外温度（单位：℃）', '室外湿度（单位：％）']
+    device_name_list1 = ['天气预报-温度', '天气预报-湿度']
+    for j in range(0, len(position_name_list1)):
+        temp_data2 = GetNearestDataFromDB(position_name=position_name_list1[j],
+                                          device_name=device_name_list1[j],
+                                          system_name='室外环境', target_time=same_time,
+                                          system_id='0501', row_name=','.join(row_name1),
+                                          table_name=env_sheet,
+                                          time='load_time',
+                                          start_time=None)
+        load_time1 = temp_data2['load_time'].values.flatten()
+        load_value1 = temp_data2['load_value'].values.flatten()
+        # 对异常值进行处理
+        processed_data = []  # 存储处理后的数据
+        for i in range(len(load_value1)):
+            if load_value1[i] < -100:
+                processed_data.append(load_value1[i-1])
+            else:
+                processed_data.append(load_value1[i])
+        processed_data = np.array(processed_data)
+        temp_data2 = pd.DataFrame()
+        temp_data2['load_time'] = load_time1
+        temp_data2['load_value'] = processed_data
+        # 按照时间进行排序
+        temp_data2 = temp_data2.groupby('load_time', as_index=False, sort=True).mean()
+        """
+        因为数据长度不一样，所以进行裁剪
+        """
+        temp_data2 = temp_data2[:-4]
+        raw_data1[j] = temp_data2['load_value']
+    raw_data1 = raw_data1.fillna(0)
+    # print(raw_data1)
+
+    # # 降采样处理得到间隔为1小时的数据
+    # raw_data1['load_time'] = pd.to_datetime(raw_data1['load_time'])
+    # raw_data1.set_index('load_time', inplace=True)
+    # # 降采样为1小时数据，保留每小时内的第一个值
+    # raw_data1 = raw_data1.resample('1H').first().shift(1, freq='10min')
+    # raw_data1 = raw_data1.reset_index(drop=False)
 
     arr = np.array(raw_data1['load_time'])
     # 将ndarray转换为DataFrame
@@ -28,39 +105,30 @@ try:
     df['month'] = df['datetime'].dt.month
     df['day'] = df['datetime'].dt.day
     df['hour'] = df['datetime'].dt.hour
-    df['load_value'] = raw_data1['load_value'][::-1].astype(float)
-    # 从环境系统数据库读入数据
-    same_time = raw_data1['load_time']
-    print(raw_data1)
-    raw_name2 = ['light_intensity', 'temperature', 'huimidity']
-    raw_data2 = GetNearestDataFromDB(position_name='a', position_id='1', device_name='b', device_id='2',
-                                     system_name='c', target_time=same_time,
-                                     system_id='3', row_name=','.join(raw_name2), table_name='sheet2', time='load_time',
-                                     start_time=None)
-    raw_data2_re = raw_data2[::-1]
-    print(raw_data2_re)
-    df['light_intensity'] = raw_data2_re['light_intensity']
-    df['temperature'] = raw_data2_re['temperature']
-    df['huimidity'] = raw_data2_re['huimidity']
-    df['light_intensity'] = df['light_intensity'].values[::-1].astype(float)
-    df['temperature'] = df['temperature'].values[::-1].astype(float)
-    df['huimidity'] = df['huimidity'].values[::-1].astype(float)
+    df['load_value'] = raw_data1['load_value'].astype(float)
+
+    df['temperature'] = raw_data1[0].astype(float)
+    df['humidity'] = raw_data1[1].astype(float)
     raw_data = df
-    print(raw_data)
+    # print(raw_data)
 except Exception as e:
     logging.error("读取失败，失败原因为")
     logging.error(traceback.format_exc())
     raise e
+finally:
+    mydb.close()
 logging.info("读取成功")
 
 # 对读取数据进行修改
+raw_data = raw_data.iloc[-4:, :]
 time_before = raw_data['datetime']
-new_order = ['year', 'month', 'day', 'hour', 'load_value', 'light_intensity', 'temperature',
-             'huimidity']  # 将"..."替换为原来的列名及其顺序
+time_before = time_before.reset_index(drop=True)
+new_order = ['year', 'month', 'day', 'hour', 'load_value', 'temperature',
+             'humidity']  # 将"..."替换为原来的列名及其顺序
 raw_data = raw_data[new_order]
-raw_data = raw_data.iloc[0:4, 1:]
-raw_data = raw_data[::-1]
-print(raw_data)
+raw_data = raw_data.iloc[:, 1:]
+# print(raw_data)
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 data_tensor = torch.tensor(raw_data.values)  # 转换为张量
 data_tensor = data_tensor.unsqueeze(0).float().clone().detach().to(device)  # 增加一个维度以匹配模型的输入形状
@@ -68,28 +136,34 @@ output = Lstm_Net(data_tensor)  # 根据模型预测结果
 output = output[-window_label_size:, :]  # 取输出序列中最后的 window_label_size 个时间步
 predictions = output.detach().cpu().numpy()  # 保存预测数据
 
-predict_time = [(time_before + datetime.timedelta(hours=4))]
+predict_time = [(time_before + timedelta(hours=4))]
 predictions = [inner_list[0] for outer_list in predictions for inner_list in outer_list]
 for i in range(len(predictions)):
     predictions[i] = predictions[i].astype(float)
-print(predictions)
+# print(predictions)
+
 # 将预测结果放入数据库
+
 try:
     logging.info("插入数据")
-    data_list = []
     for i in range(4):
-        data_dict = {'system_id': '3', 'system_name': 'c', 'actual_time': predict_time[0][i],
-                     'forcast_value': predictions[4-i-1], 'forcast_type': '4'}
-        data_list.append(data_dict)
-    print(data_list)
-    for data_dict in data_list:
-        if IsExist(data_dict) > 0:
-            UpdateData(table_name='sheet3', data_dict=data_dict, system_name='c', system_id='3',
-                       actual_time=data_dict['actual_time'])
+        data_dict = {'system_id': '10', 'system_name': '光伏发电量', 'actual_time': predict_time[0][i],
+                     'forecast_value': predictions[i], 'forecast_type': '4'}
+        print(data_dict)
+        # 读取现有预测数据
+        predict_data_ori = np.array(
+            GetPredictDataFromDB(row_name='actual_time', table_name=output_sheet, actual_time='actual_time',
+                                 system_id='10', system_name='光伏发电量', forecast_type=data_dict['forecast_type']))
+        cur = data_dict['actual_time']
+        if cur not in predict_data_ori:
+            InsertData(table_name=output_sheet, data_dict=data_dict)
         else:
-            InsertData(table_name='sheet3', data_dict=data_dict)
+            UpdateData(table_name=output_sheet, data_dict=data_dict, system_name='光伏发电量', system_id='10',
+                       actual_time=data_dict['actual_time'])
 except Exception as e:
     logging.error("插入数据失败, 失败原因为")
     logging.error(traceback.format_exc())
     raise e
+finally:
+    mydb.close()
 logging.info("数据插入成功")
