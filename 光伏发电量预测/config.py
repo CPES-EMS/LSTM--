e7828bc820619bf8181b062f391ab949a581b3ae
jiahao666-chen <88417432+jiahao666-chen@users.noreply.github.com>
2023-12-22 20:12:51 +0800

    Update config.py

--- 光伏发电量预测/config.py
+++ 光伏发电量预测/config.py
@@ -6,28 +6,31 @@ import torch  # 导入 PyTorch 库
 # 日志记录设置
 logging = TNLog(dir='log', name='date at ' + time.strftime('%Y%m%d', time.localtime()))
 # 定义参数
-window_input_size = 24 * 4  # 输入窗口大小，即用多少小时的数据来预测未来
-window_label_size = 4  # 输出窗口大小，即预测未来多少个小时的数据
-epoch = 1000  # 训练轮数
+window_input_size = 24 * 14  # 输入窗口大小，即用多少小时的数据来预测未来
+window_label_size = 24  # 输出窗口大小，即预测未来多少个小时的数据
+epoch = 3000  # 训练轮数
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")  # 设置设备类型，如果有 GPU 则使用 GPU，否则使用 CPU
 batch_size = 16  # 批量大小,一次训练的样本数
 
-input_size = 7  # 模型输入维度
+input_size = 6  # 模型输入维度
 hidden_size = 128  # 隐层维度
 output_size = 1  # 模型输出维度
 
 """数据库信息"""
-dataaddress="rm-2ze568o4qd438xw6cdo.mysql.rds.aliyuncs.com"  # 数据库主机地址
-port_num="3306"  # 数据库端口号
-user_name="yuce"  # 数据库用户名
-password="yuce@123456"  # 数据库密码
-datause="yuce"
+dataaddress="123.249.70.226"  # 数据库主机地址
+port_num="7047"  # 数据库端口号
+user_name="root"  # 数据库用户名
+password="ems123@4"  # 数据库密码
+datause="ems"
 
 """数据库对接表"""
-test_sheet = "ems_els_new_energy_electricity_forecast"
+hvac_sheet = "ems_hvac_history"
+els_sheet = "ems_els_history"
+hydrogen_sheet = "ems_hydrogen_history"
+env_sheet = "ems_environment_history"
 output_sheet = "ems_els_new_energy_electricity_forecast"
 
 """关口信息"""
-area_id_1 = "DBC0441E15FDJ_310"
-area_id_2 = "EAB020A_05B___302"
-area_id_3 = "EAA050A222B___302"
+area_id_1 = ""
+area_id_2 = ""
+area_id_3 = ""
