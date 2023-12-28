dac15599f6c5c3252c06a411ba3500fc6602056f
jiahao666-chen <88417432+jiahao666-chen@users.noreply.github.com>
2023-12-28 13:09:22 +0800

    Update config.py

--- 冷（热）负荷预测/config.py
+++ 冷（热）负荷预测/config.py
@@ -6,9 +6,9 @@ import torch  # 导入 PyTorch 库
 # 日志记录设置
 logging = TNLog(dir='log', name='date at ' + time.strftime('%Y%m%d', time.localtime()))
 # 定义参数
-window_input_size = 24 * 16  # 输入窗口大小，即用多少小时的数据来预测未来
-window_label_size = 24  # 输出窗口大小，即预测未来多少个小时的数据
-window_gap_size = 24*3
+window_input_size = 24 * 6  # 输入窗口大小，即用多少小时的数据来预测未来
+window_label_size = 4  # 输出窗口大小，即预测未来多少个小时的数据
+window_gap_size = 12
 epoch = 12000  # 训练轮数
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")  # 设置设备类型，如果有 GPU 则使用 GPU，否则使用 CPU
 batch_size = 16  # 批量大小,一次训练的样本数
@@ -17,6 +17,7 @@ input_size = 9  # 模型输入维度
 hidden_size = 128  # 隐层维度
 output_size = 1  # 模型输出维度
 
+# ems 数据库
 """数据库信息"""
 dataaddress="123.249.70.226"  # 数据库主机地址
 port_num="7047"  # 数据库端口号
@@ -35,3 +36,10 @@ area_id_1 = ""
 area_id_2 = ""
 area_id_3 = ""
 
+# # 阿里云数据库
+# """数据库信息"""
+# dataaddress = "192.168.3.13"
+# port_num = "3306"
+# user_name = "root"
+# password = "123456"
+# datause = "ems"
