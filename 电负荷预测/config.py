f9a7ad9fb5a60c4c969d666d1863678c31cd6be9
jiahao666-chen <88417432+jiahao666-chen@users.noreply.github.com>
2023-12-28 13:12:30 +0800

    Update config.py

--- 电负荷预测/config.py
+++ 电负荷预测/config.py
@@ -6,9 +6,9 @@ import torch  # 导入 PyTorch 库
 # 日志记录设置
 logging = TNLog(dir='log', name='date at ' + time.strftime('%Y%m%d', time.localtime()))
 # 定义参数
-window_input_size = 24 * 14  # 输入窗口大小，即用多少小时的数据来预测未来
-window_label_size = 24  # 输出窗口大小，即预测未来多少个小时的数据
-window_gap_size = 24*3
+window_input_size = 24 * 4  # 输入窗口大小，即用多少小时的数据来预测未来
+window_label_size = 4  # 输出窗口大小，即预测未来多少个小时的数据
+window_gap_size = 12  # 滑动窗口大小
 epoch = 1000  # 训练轮数
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")  # 设置设备类型，如果有 GPU 则使用 GPU，否则使用 CPU
 batch_size = 16  # 批量大小,一次训练的样本数
@@ -34,3 +34,12 @@ output_sheet = "ems_els_electric_load_forecast"
 area_id_1 = ""
 area_id_2 = ""
 area_id_3 = ""
+
+
+# # 阿里云数据库
+# """数据库信息"""
+# dataaddress = "192.168.3.13"
+# port_num = "3306"
+# user_name = "root"
+# password = "123456"
+# datause = "ems"
